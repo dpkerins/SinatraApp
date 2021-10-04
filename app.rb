2522614a0331306d348f972461b98501d056508f
LANGAN84 <langan.kg@gmail.com>
2021-10-04 16:35:50 +0100

    Introducing params to the app

--- app.rb
+++ app.rb
@@ -17,8 +17,13 @@ get '/kirstie' do
   "This is Kirstie's route"
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = ["Amigo", "Misty", "Almond"].sample 
   erb(:index)
 end
 
+get '/named-cat' do 
+  p params
+  @cat_name = params[:name]
+  erb(:index)
+end
